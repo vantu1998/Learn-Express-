6d83393a8d8f238a113a131ffe0f707347e4a282
Tu Nguyen <vantu1998@gmail.com>
2020-04-04 15:38:41 +0700

    create user validate and custom template

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -27,6 +27,22 @@ module.exports.create = (req,res)=>{
 
 module.exports.postCreate = (req,res)=>{
     req.body.id = shortid.generate();
+    var errors = [];
+    var values = [];
+
+    if(!req.body.name){
+        errors.push('Name is require.')
+    }
+    if(!req.body.phone){
+        errors.push("Phone is require.")
+    }
+    if(errors.length){
+        res.render('users/create',{
+            errors: errors,
+            values: req.body
+        })
+        return;
+    }
     db.get('users').push(req.body).write();
     res.redirect('/users');
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 var port = 3000;
 var userRoutes = require('./routes/user.route');
 
+app.use(express.static('public'));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 // setup view engine
@@ -13,7 +14,7 @@ app.set('views','./views')
 app.get('/',function(request,response){
     response.render('index',{
         // truyen bien qua index.pug de su dung
-        name: 'My friend'
+        name: 'wellcome'
     })
 })
 
